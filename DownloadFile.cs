6bcfe9fb2d363230f7d0e4e865dd296f9de82add
raja150 <60502835+raja150@users.noreply.github.com>
2023-08-31 11:08:28 +0530

    Add files via upload

---
+++ DownloadFile.cs
@@ -0,0 +1,17 @@
+﻿using HeyRed.Mime;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace TranSmart.API.Extensions
+{
+    public class DownloadFile : FileStreamResult
+    {
+        public DownloadFile(MemoryStream data, string filename) :
+            base(data, MimeTypesMap.GetMimeType(Path.GetExtension(filename)))
+        {
+            data.Seek(0, SeekOrigin.Begin);
+            base.FileDownloadName = filename;
+        }
+    }
+
+}
